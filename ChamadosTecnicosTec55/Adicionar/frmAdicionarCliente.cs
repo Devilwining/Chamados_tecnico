f4cd8c25e58e099f3697f865b6084854c0030cb2
Devilwining <159485044+Devilwining@users.noreply.github.com>
2024-03-21 17:18:01 -0300

    cadastro de cliente
    Conectamos o dao ao cliente para então
    incluir no baco de dados.

--- ChamadosTecnicosTec55/Adicionar/frmAdicionarCliente.cs
+++ ChamadosTecnicosTec55/Adicionar/frmAdicionarCliente.cs
@@ -41,6 +41,33 @@ namespace ChamadosTecnicosTec55.Adicionar
             {
                 MessageBox.Show("Campos em branco obrigatorio");
             }
+            else 
+            {
+                // TODA VEZ QUE MEXER COM BD USUAR TRY
+                try 
+                {
+
+                    //Preencher o Objeto Cliente
+                    cliente.Nome = txbNome.Text;
+                    cliente.Profissao = txbProfissao.Text;
+                    cliente.Obs = txbObs.Text;
+                    cliente.Setor = txbSetor.Text;
+
+                    // CHAMA O DAO para incluir o cliente
+                    clientedao.IncluiCliente(cliente);
+                }catch (Exception ex) 
+                {
+                    MessageBox.Show("Erro ao Cadastrar"+ex,"Atenção",
+                                     MessageBoxButtons.OK,
+                                     MessageBoxIcon.Error);           
+                }
+                finally 
+                {
+                    MessageBox.Show("Cadastrado com sucesso !");
+                }
+            
+            
+            }
         }
         
 
